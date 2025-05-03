55cd5d7a33d990227253fdd3a873c2db8e78e962
Aaron Jumarang <44382744+aaronjuma@users.noreply.github.com>
2025-05-03 15:50:47 -0400

    blinking lights

---
+++ 2DChess-main/Assets/DataCollector.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataCollector : MonoBehaviour
+{
+    [SerializeField] public SSVEP_Blink stim1;
+    [SerializeField] public SSVEP_Blink stim2;
+    [SerializeField] public SSVEP_Blink stim3;
+    [SerializeField] public SSVEP_Blink stim4;
+    [SerializeField] public SSVEP_Blink stim5;
+    [SerializeField] public SSVEP_Blink stim6;
+    [SerializeField] public SSVEP_Blink stim7;
+    [SerializeField] public SSVEP_Blink stim8;
+
+    private float stimulusTime = 4;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Selection());
+        }
+    }
+
+    IEnumerator Selection() {
+        stim1.StartBlink();
+        stim2.StartBlink();
+        stim3.StartBlink();
+        stim4.StartBlink();
+        stim5.StartBlink();
+        stim6.StartBlink();
+        stim7.StartBlink();
+        stim8.StartBlink();
+
+        yield return new WaitForSeconds(4);
+
+        stim1.StopBlink();
+        stim2.StopBlink();
+        stim3.StopBlink();
+        stim4.StopBlink();
+        stim5.StopBlink();
+        stim6.StopBlink();
+        stim7.StopBlink();
+        stim8.StopBlink();
+
+        yield return new WaitForSeconds(4);
+    }
+}
---
+++ 2DChess-main/Assets/SSVEP_Blink.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SSVEP_Blink : MonoBehaviour
+{
+    private float period;
+    private float blinkingTime;
+    
+    [SerializeField] public float frequency;
+    [SerializeField] public float phaseShift = 0; //Radians (pi)
+    [SerializeField] private Color off;
+    [SerializeField] private Color on;
+
+    public SpriteRenderer image;
+    private bool blinking;
+    private float startTime;
+
+    // Start is called before the first frame update
+    void Start() {
+        period = 1f/frequency;
+        blinking = false;
+    }
+
+    void Update() {
+        if (blinking) {
+            Blink();
+        }
+    }
+
+    public void StartBlink() {
+        blinking = true;
+        startTime = Time.time;
+    }
+
+    public void StopBlink() {
+        blinking = false;
+        image.color = off;
+    }
+
+    // Update is called once per frame
+    private void Blink() {
+        float colorMix = Mathf.InverseLerp(-1f, 1f, Mathf.Sin(((Time.time-startTime) % 10.0f) * Mathf.PI * frequency * 2.0f + phaseShift*Mathf.PI));
+        image.color = Color.Lerp(off, on, colorMix);
+    }
+}
